fb480af7d37686259196dc09bf05229dbc710fbf
hangjin <hangs0908@gmail.com>
2024-02-03 14:45:05 +0900

    fix : ItCookUser 엔티티 수정(빌더패턴)

--- itcook-domain/src/main/java/com/itcook/cooking/domain/domains/user/entity/ItCookUser.java
+++ itcook-domain/src/main/java/com/itcook/cooking/domain/domains/user/entity/ItCookUser.java
@@ -2,13 +2,17 @@ package com.itcook.cooking.domain.domains.user.entity;
 
 import com.itcook.cooking.domain.common.BaseTimeEntity;
 import com.itcook.cooking.domain.domains.user.enums.UserRole;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "itcook_user")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ItCookUser extends BaseTimeEntity {
 
     @Id
@@ -20,4 +24,12 @@ public class ItCookUser extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
+    @Builder
+    public ItCookUser(Long id, String email, String password, String nickName, UserRole userRole) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickName = nickName;
+        this.userRole = userRole;
+    }
 }
