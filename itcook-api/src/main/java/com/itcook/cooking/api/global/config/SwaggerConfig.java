3a118e270ad5ed75475a61c1749a26040d967d52
hangjin <hangs0908@gmail.com>
2024-02-07 16:14:58 +0900

    fix : Swagger관련 설정 추가

--- itcook-api/src/main/java/com/itcook/cooking/api/global/config/SwaggerConfig.java
+++ itcook-api/src/main/java/com/itcook/cooking/api/global/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.itcook.cooking.api.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -9,6 +11,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        }
+)
 public class SwaggerConfig {
 
 //    @Bean
