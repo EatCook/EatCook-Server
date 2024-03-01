62f5942a2221164553c1078922f8c7fccb9ed147
hangjin <hangs0908@gmail.com>
2024-03-01 13:24:26 +0900

    refactor : 추가 회원가입 요청에 대한 리팩토링

--- itcook-domain/src/main/java/com/itcook/cooking/domain/domains/user/service/UserDomainService.java
+++ itcook-domain/src/main/java/com/itcook/cooking/domain/domains/user/service/UserDomainService.java
@@ -37,20 +37,33 @@ public class UserDomainService {
     @Transactional
     public ItCookUser addSignup(ItCookUser user, List<CookingType> cookingTypes) {
         //닉네임 중복 체크
-        userRepository.findByNickName(user.getNickName())
-            .ifPresent(it -> {
-                throw new ApiException(UserErrorCode.ALREADY_EXISTS_NICKNAME);
-            });
+        checkDuplicatedNicName(user);
         // 업데이트 필드 (닉네임, 거주형태)
-        ItCookUser itCookUser = userRepository.findById(user.getId())
-            .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
-        itCookUser.updateItCookUser(user);
+        ItCookUser itCookUser = updateNickNameAndLifeType(user);
 
+        createCookingThemes(user, cookingTypes);
+
+        return itCookUser;
+    }
+
+    private void createCookingThemes(ItCookUser user, List<CookingType> cookingTypes) {
         cookingTypes.forEach(cookingType -> {
             CookingTheme cookingTheme = CookingTheme.createCookingTheme(user.getId(), cookingType);
             cookingThemeRepository.save(cookingTheme);
         });
+    }
 
+    private ItCookUser updateNickNameAndLifeType(ItCookUser user) {
+        ItCookUser itCookUser = userRepository.findById(user.getId())
+            .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
+        itCookUser.updateItCookUser(user);
         return itCookUser;
+    }
+
+    private void checkDuplicatedNicName(ItCookUser user) {
+        userRepository.findByNickName(user.getNickName())
+            .ifPresent(it -> {
+                throw new ApiException(UserErrorCode.ALREADY_EXISTS_NICKNAME);
+            });
     }
 }
