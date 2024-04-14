206edbb8d94bfe7563f70ae0918b8c5f2ccc26c2
Chanwon-Seo <scwonn60@gmail.com>
2024-04-14 18:06:44 +0900

    feat : Follow swagger 수정

--- itcook-api/src/main/java/com/itcook/cooking/api/domains/user/controller/FollowController.java
+++ itcook-api/src/main/java/com/itcook/cooking/api/domains/user/controller/FollowController.java
@@ -5,6 +5,7 @@ import com.itcook.cooking.api.domains.user.dto.request.follow.FollowRequest;
 import com.itcook.cooking.api.domains.user.service.FollowUseCase;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-@Slf4j
-@Tag(name = "01. User")
+@SecurityRequirement(name = "access-token")
+@Tag(name = "04. Follow")
 public class FollowController {
 
     private final FollowUseCase followUseCase;
