e38db901a003a46877f3b1f0cf6108b81e06520d
Hangjin <56857925+hangs0908@users.noreply.github.com>
2024-07-29 02:42:45 +0900

    fix : 관심요리 설정 스웨거 수정

--- itcook-api/src/main/java/com/itcook/cooking/api/domains/user/dto/request/UserUpdateInterestCookRequest.java
+++ itcook-api/src/main/java/com/itcook/cooking/api/domains/user/dto/request/UserUpdateInterestCookRequest.java
@@ -9,7 +9,8 @@ import javax.validation.constraints.Size;
 
 @Schema(name = "관심 요리 설정 요청")
 public record UserUpdateInterestCookRequest(
-    @Schema(description = "생활 유형", example = "다이어트만 n년째")
+    @Schema(description = "생활 유형(설정을 안할시 필수값 아님)",
+        example = "다이어트만 n년째 or 편의점은 내 구역 or 건강한 식단관리 or 밀키트 lover or 배달음식 단골고객")
     String lifeType,
     @Size(max = 3, message = "최대 3개까지 선택 가능합니다.")
     @Schema(description = "요리 유형", example = "[\"한식\", \"중식\", \"일식\"]")
