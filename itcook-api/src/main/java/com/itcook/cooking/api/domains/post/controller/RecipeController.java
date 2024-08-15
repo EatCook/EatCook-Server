aa6bfb914e1abe22b8520def05f58b93a95f7637
Chanwon-Seo <scwonn60@gmail.com>
2024-08-15 14:19:50 +0900

    refactor : 레시피 삭제 http 메서드 업데이트

--- itcook-api/src/main/java/com/itcook/cooking/api/domains/post/controller/RecipeController.java
+++ itcook-api/src/main/java/com/itcook/cooking/api/domains/post/controller/RecipeController.java
@@ -17,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,7 +76,7 @@ public class RecipeController {
     }
 
     @Operation(summary = "recipe 삭제 요청", description = "recipe 삭제 요청 설명")
-    @PostMapping("/v1/recipes/{recipeId}")
+    @DeleteMapping("/v1/recipes/{recipeId}")
     public ResponseEntity<ApiResponse<String>> removeRecipe(
             @AuthenticationPrincipal AuthenticationUser authenticationUser,
             @PathVariable Long recipeId
