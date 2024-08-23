e14a8235499357d40845132b50239e574dc1bf0c
Chanwon-Seo <scwonn60@gmail.com>
2024-08-24 00:14:45 +0900

    refactor : 레시피 조리 최소 시간 업데이트

--- itcook-domain/src/main/java/com/itcook/cooking/domain/common/constant/PostConstant.java
+++ itcook-domain/src/main/java/com/itcook/cooking/domain/common/constant/PostConstant.java
@@ -4,7 +4,7 @@ public abstract class PostConstant {
     public static final int RECIPE_NAME_MAX_SIZE = 100;
     public static final int RECIPE_INTRODUCTION_MAX_SIZE = 100;
     public static final int RECIPE_MAX_TIME = 86_400;
-    public static final int RECIPE_MIN_TIME = 60;
+    public static final int RECIPE_MIN_TIME = 1;
     public static final int RECIPE_PROCESS_MIN_STEP_NUM = 1;
     public static final int RECIPE_FOOD_INGREDIENTS_VALUE_MAX_SIZE = 255;
     public static final int RECIPE_FOOD_INGREDIENTS_VALUE_MIN_SIZE = 1;
